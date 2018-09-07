2aeae5fe39d8ecd4ed8460abcfc42ea2a3711221
Ryan M. Waite <ryanwaite28@gmail.com>
2018-09-07 11:56:55 -0400

    Added "ResetPasswordRequests" Model

--- server/models.js
+++ server/models.js
@@ -50,6 +50,12 @@ models.UserFields = sequelize.define('user_fields', {
   unique_value:    { type: Sequelize.STRING, unique: true, defaultValue: chamber.uniqueValue }
 }, { freezeTableName: true, indexes: [{ unique: true, fields: ['unique_value'] }] });
 
+models.ResetPasswordRequests = sequelize.define('reset_password_requests', {
+  user_email:      { type: Sequelize.INTEGER, allowNull: false, references: { model: models.Users, key: 'email' } },
+  date_created:    { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
+  unique_value:    { type: Sequelize.STRING, unique: true, defaultValue: chamber.greatUniqueValue }
+}, { freezeTableName: true, indexes: [{ unique: true, fields: ['user_email', 'unique_value'] }] });
+
 
 
 models.Entities = sequelize.define('entities', {
